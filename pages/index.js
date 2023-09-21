00f325f3e271907ff46af50bb8795cd6015b0a68
Stefan Ylimäki <stefan.ylimaki@hillagroup.fi>
2023-09-21 09:42:38 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// init your index.js
+import React from "react";
+import Layout from "../components/layout";
+
+function index() {
+  return <Layout>Hello World!</Layout>;
+}
+
+export default index;
